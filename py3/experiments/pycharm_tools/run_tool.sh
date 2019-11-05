04d8ff363e0af6221e19039a3bd4710f571a225a
alon <alon@optibus.com>
2019-11-05 19:15:40 +0200

    remove experiments

--- py3/experiments/pycharm_tools/run_tool.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH="/home/alon/anaconda3/bin:$PATH"
-
-export PYTHONPATH='/home/alon/scripts/py3'
-
-python /home/alon/scripts/py3/experiments/pycharm_tools/_001.py $*
