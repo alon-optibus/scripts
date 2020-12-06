6ce39d198970299ea4cac2f078df6886e8adce83
alon <alon@optibus.com>
2020-12-06 12:20:50 +0200

    automation

--- bash/procedures/chrome/new_tab_bookmark.sh
+++ bash/procedures/chrome/new_tab_bookmark.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+WM_CLASS_CHROME="google-chrome"
+
 source wm_utils_1.sh
 
-if [ "$wm_class" = '"google-chrome"' ]
+if [ "$wm_class" = "$WM_CLASS_CHROME" ]
 then
 
   sleep 0.2
 
   xdotool mousemove 0 0
 
-  if [ "$wm_name" != '"New Tab - Google Chrome"' ]
+  if [ "$wm_name" != "New Tab - Google Chrome" ]
   then
     xdotool key ctrl+t
   fi
