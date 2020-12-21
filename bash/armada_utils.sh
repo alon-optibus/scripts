fe937fde3348572ee78362ba3c776d33431986be
alon <alon@optibus.com>
2020-12-21 23:35:18 +0200

    env_euclid

--- bash/armada_utils.sh
+++ bash/armada_utils.sh
@@ -7,9 +7,11 @@ source $ROOT/.bash_profile
 alias a='cdi $ROOT'
 alias r='cdi $RESEARCH'
 
+alias env_euclid='set -a; source "$LOCAL/env_euclid.env"; set +a'
+
 pip_install_requirements(){
     pipu
-    pip2r "$ROOT/euclid/requirements.txt"
+    pip2pr "$ROOT/euclid/requirements.txt"
     pip2cr "$ROOT/euclid/requirements.txt"
     pip2cr "$ROOT/euclid/cpython_requirements.txt"
     pip2vr "$ROOT/euclid/requirements.txt"
